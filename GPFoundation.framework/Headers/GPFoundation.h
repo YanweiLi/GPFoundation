a34b305ab6b0799571e130841c13e83c939e277a
Liyanwei <yifan.lyw@alibaba-inc.com>
2018-08-30 17:36:27 +0800

    提交1.0.0版本

---
+++ GPFoundation.framework/Headers/GPFoundation.h
@@ -0,0 +1,11 @@
+//
+//  GPFoundation.m
+//  GPFoundation
+//
+//  Created by Liyanwei on 2018/8/30.
+//  Copyright © 2018年 Liyanwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <GPFoundation/GPTuple+GPTransfer.h>
---
+++ GPFoundation.framework/Headers/GPTuple+GPTransfer.h
@@ -0,0 +1,15 @@
+//
+//  GPTuple+GPTransfer.h
+//  GPFoundation
+//
+//  Created by Liyanwei on 2018/8/30.
+//  Copyright © 2018年 Liyanwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GPTuple/GPTuple.h>
+#import <GrapeSequence/GrapeSequence.h>
+
+@interface GPTupleBase (GPSTransfer) <GPSTransfer>
+
+@end
